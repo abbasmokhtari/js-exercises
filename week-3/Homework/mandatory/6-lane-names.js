6770eaf9fd9dd458b226b8c28150087e93f698a7
Abbas Mokhtari <abs.mokhtari@gmail.com>
2020-03-13 19:52:35 +0000

    Lane Names finished

--- week-3/Homework/mandatory/6-lane-names.js
+++ week-3/Homework/mandatory/6-lane-names.js
@@ -4,8 +4,8 @@
   Write a function that will return all street names which contain 'Lane' in their name.
 */
 
-function getLanes() {
-
+function getLanes(arr) {
+    return arr.filter(word => word.indexOf(' Lane') > 0);
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
